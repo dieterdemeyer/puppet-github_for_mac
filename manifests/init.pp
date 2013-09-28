90619f5c3807c0c01893e3233fb67822b9e4ac7b
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-28 15:46:59 +0200

    Updated application

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class github_for_mac {
   package { 'Github for Mac':
     provider => 'compressed_app',
-    source   => 'https://github-central.s3.amazonaws.com/mac%2FGitHub%20for%20Mac%20111.zip',
+    source   => 'https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20160.zip',
   }
 }
